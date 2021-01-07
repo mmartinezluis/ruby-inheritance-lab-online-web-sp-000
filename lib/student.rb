56860d689be71207b39b312cdaa943ae7d8d94c8
Luis Martinez <luis_mmartinez@live.com>
2021-01-07 02:47:45 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,16 @@
-class Student 
+class Student < User
+    attr_accessor :knowledge
 
+  def initialize
+    @knowledge = [ ]
+  end
+
+  def learn(teacher.teach)
+    
+    @knowledge << teacher.teach
+  end
+
+  def knowledge
+    @knowledge
+  end
 end
